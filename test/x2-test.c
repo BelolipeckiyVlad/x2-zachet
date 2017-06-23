ae1f91f4b8c1b0ba03e985285fc82ed1c0cb1daa
Белолипецкий Владислав Александрович <ip613s05@awp08.csc.local>
2017-06-23 14:07:11 +0700

    test dec

--- test/x2-test.c
+++ test/x2-test.c
@@ -1,3 +1,35 @@
 #include <ctest.h>
 #include <../src/x2.h>
+#include <math.h>
+
+CTEST(Test_1_Dec, input_1_2_1)
+{
+    double result = Dec(1,2,1);
+	
+    double expected = 0;
+	
+    ASSERT_DBL_NEAR(expected, result);
+}
+
+CTEST(Test_2_Dec, input_2_10_3)
+{
+    double result = Dec(2,10,3);
+	
+    double expected = 76;
+	
+    ASSERT_DBL_NEAR(expected, result);
+}
+
+CTEST(Test_3_Dec, input_5_1_1)
+{
+    double result = Dec(5,1,1);
+	
+    double expected = -19;
+	
+    ASSERT_DBL_NEAR(expected, result);
+}
+
+
+
+
 
