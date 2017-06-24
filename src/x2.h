407c4173dcc4590904ea98e3c11f93d65ec40e0c
Белолипецкий Владислав Александрович <ip613s05@awp12.csc.local>
2017-06-24 10:10:09 +0700

    change 2.3(x2.h)

--- src/x2.h
+++ src/x2.h
@@ -6,4 +6,10 @@ double Dec(double a, double b, double c);
 double Dec0(double b, double a);
 
 double Dec1(double a, double b, double D);
+
+double Dec2(double a, double b, double D);
+
+double kofa0(double b);
+
+double kofb0(double a);
 #endif
