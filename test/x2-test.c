4757be3310908efd89cbd867ac7249eb8ccf3db5
Белолипецкий Владислав Александрович <ip613s05@awp12.csc.local>
2017-06-24 11:27:37 +0700

    final test

--- test/x2-test.c
+++ test/x2-test.c
@@ -75,16 +75,7 @@ CTEST(Test_7_Dec0, input_8_1)
     ASSERT_DBL_NEAR(expected, result);
 }
 
-CTEST(Test_8_Dec1, input_8_1)
-{
-    double result = Dec1(8,1);
-	
-    double expected = 0;
-	
-    ASSERT_DBL_NEAR(expected, result);
-}
-
-CTEST(Test_9_Dec1, input_1_8_4)
+CTEST(Test_8_Dec1, input_1_8_4)
 {
     double result = Dec1(1,8,4);
 	
@@ -93,7 +84,7 @@ CTEST(Test_9_Dec1, input_1_8_4)
     ASSERT_DBL_NEAR(expected, result);
 }
 
-CTEST(Test_10_Dec1, input_1_1_9)
+CTEST(Test_9_Dec1, input_1_1_9)
 {
     double result = Dec1(1,1,9);
 	
@@ -102,7 +93,7 @@ CTEST(Test_10_Dec1, input_1_1_9)
     ASSERT_DBL_NEAR(expected, result);
 }
 
-CTEST(Test_11_Dec2, input_1_5_25)
+CTEST(Test_10_Dec2, input_1_5_25)
 {
     double result = Dec2(1,-5,25);
 	
@@ -111,7 +102,7 @@ CTEST(Test_11_Dec2, input_1_5_25)
     ASSERT_DBL_NEAR(expected, result);
 }
 
-CTEST(Test_12_Dec2, input_2_10_4)
+CTEST(Test_11_Dec2, input_2_10_4)
 {
     double result = Dec2(2,-10,4);
 	
@@ -120,7 +111,7 @@ CTEST(Test_12_Dec2, input_2_10_4)
     ASSERT_DBL_NEAR(expected, result);
 }
 
-CTEST(Test_13_Dec2, input_3_2_100)
+CTEST(Test_12_Dec2, input_3_2_100)
 {
     double result = Dec2(3,2,100);
 	
