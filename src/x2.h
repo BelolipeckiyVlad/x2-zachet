5e5715dd212b32c136de3c9990231da01c2f4941
Белолипецкий Владислав Александрович <ip613s05@awp12.csc.local>
2017-06-24 10:27:59 +0700

    2.6 (x2.h)

--- src/x2.h
+++ src/x2.h
@@ -9,7 +9,7 @@ double Dec1(double a, double b, double D);
 
 double Dec2(double a, double b, double D);
 
-double kofa0(double b);
+double kofa0(double b, double c);
 
-double kofb0(double a);
+double kofb0(double a, double c);
 #endif
